15d889d624178f324ea65c22f3f1d0826d9435ab
Aman Khalid <mailamankhalid@gmail.com>
2018-12-02 10:57:45 +0530

    First go

--- remove-nth-node-from-end-of-list.py
+++ remove-nth-node-from-end-of-list.py
@@ -4,6 +4,7 @@ class ListNode:
         self.val = x
         self.next = None
 
+
 class Solution:
     def removeNthFromEnd(self, head, n):
         """
@@ -11,8 +12,28 @@ class Solution:
         :type n: int
         :rtype: ListNode
         """
+        if head.next==None:
+            return None
+        def find_len(node, i = 1):
+            if node.next is None:
+                return i
+            return find_len(node.next, i+1)
 
-def create_list(current_node, next_node, i, arr = []):
+        def pop_ops(current_pos, popy, node):
+            if current_pos == popy:
+                if node.next.next is None:
+                    node.next = None
+                else:
+                    node.next = node.next.next
+                    return
+            pop_ops(current_pos+1, popy, node.next)
+
+        pop_at = find_len(head) - n
+        pop_ops(1, pop_at, head)
+        return head
+
+
+def create_list(current_node, next_node, i, arr=[]):
     new_node = ListNode(0)
     if i == len(arr)-1:
         current_node.val = arr[i]
@@ -21,13 +42,16 @@ def create_list(current_node, next_node, i, arr = []):
     current_node.val = arr[i]
     create_list(next_node, new_node, i+1, arr)
 
+
 def print_list(node):
-    if node.next==None:
-        print (node.val)
+    if node.next is None:
+        print(node.val)
         return
     print(node.val)
     print_list(node.next)
 
+
 node = ListNode(0)
-create_list(node, ListNode(0), 0 , [1,2,3,4,5])
+create_list(node, ListNode(0), 0, [1, 2, 3, 4, 5])
+Solution().removeNthFromEnd(node, 2)
 print_list(node)
